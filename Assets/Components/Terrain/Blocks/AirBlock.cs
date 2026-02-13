3ac6cc693f95b5596fa491f1e296fbe21526b6bb
Angie Shin <jeonghee.shin@ucalgary.ca>
2026-02-13 00:32:41 -0700

    updated block logic

--- Assets/Components/Terrain/Blocks/AirBlock.cs
+++ Assets/Components/Terrain/Blocks/AirBlock.cs
@@ -26,6 +26,12 @@ namespace Antymology.Terrain
         /// </summary>
         private Dictionary<byte, double> phermoneDeposits;
 
+        /// <summary>
+        /// Lightweight pheromone values for movement bias.
+        /// </summary>
+        private float foodPheromone;
+        private float nestPheromone;
+
         #endregion
 
         #region Methods
@@ -46,13 +52,58 @@ namespace Antymology.Terrain
             throw new Exception("An invisible tile cannot have a tile map coordinate.");
         }
 
+        public float GetFoodPheromone()
+        {
+            return foodPheromone;
+        }
+
+        public float GetNestPheromone()
+        {
+            return nestPheromone;
+        }
+
+        public void AddFoodPheromone(float amount, float maxValue)
+        {
+            foodPheromone = Mathf.Min(maxValue, foodPheromone + amount);
+        }
+
+        public void AddNestPheromone(float amount, float maxValue)
+        {
+            nestPheromone = Mathf.Min(maxValue, nestPheromone + amount);
+        }
+
+        public void Decay(float rate)
+        {
+            foodPheromone = Mathf.Max(0f, foodPheromone - rate);
+            nestPheromone = Mathf.Max(0f, nestPheromone - rate);
+        }
+
         /// <summary>
-        /// THIS CURRENTLY ONLY EXISTS AS A WAY OF SHOWING YOU WHATS POSSIBLE.
+        /// Diffuses pheromone values to/from neighboring air blocks.
+        /// Each tick, a portion of this block's pheromone is shared with neighbors, and vice versa.
         /// </summary>
-        /// <param name="neighbours"></param>
+        /// <param name="neighbours">Array of adjacent blocks (should be AirBlocks).</param>
         public void Diffuse(AbstractBlock[] neighbours)
         {
-            throw new NotImplementedException();
+            // Simple diffusion: average with neighbors
+            float totalFood = foodPheromone;
+            float totalNest = nestPheromone;
+            int count = 1;
+            foreach (var block in neighbours)
+            {
+                if (block is AirBlock air)
+                {
+                    totalFood += air.foodPheromone;
+                    totalNest += air.nestPheromone;
+                    count++;
+                }
+            }
+            float avgFood = totalFood / count;
+            float avgNest = totalNest / count;
+            // Blend towards average (diffusion rate = 0.2)
+            float diffusionRate = 0.2f;
+            foodPheromone = Mathf.Lerp(foodPheromone, avgFood, diffusionRate);
+            nestPheromone = Mathf.Lerp(nestPheromone, avgNest, diffusionRate);
         }
 
         #endregion
